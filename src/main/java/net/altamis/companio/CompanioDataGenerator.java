de83e63d7c5ddffc44f3e95f1f517aaa258ef009
Altamis07 <altamisahamed2008@gmail.com>
2026-08-05 18:55:37 +0530

    Init

---
+++ src/main/java/net/altamis/companio/CompanioDataGenerator.java
@@ -0,0 +1,11 @@
+package net.altamis.companio;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CompanioDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
